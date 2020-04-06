cf51361ae67ef339abfed8a348d004702b595e3e
Nuno <n.alexandre@xgeeks.io>
2020-04-06 21:52:13 +0100

    feat(single page): add single post page

--- components/PostItem.vue
+++ components/PostItem.vue
@@ -1,36 +1,38 @@
 <template>
   <div class="post-item">
-    <img :src="post.fields.image.fields.file.url">
-    <nuxt-link :to="{name: 'posts-slug', slug: post.fields.slug }"><h2 class="title">{{ post.fields.title }}</h2></nuxt-link>
+    <img :src="post.fields.image.fields.file.url" />
+    <nuxt-link :to="'posts/' + post.fields.slug">
+      <h2 class="title">{{ post.fields.title }}</h2>
+    </nuxt-link>
     <p v-html="post.fields.description"></p>
   </div>
 </template>
 
 <script>
-    export default {
-        props: {
-          post:{
-            type: Object,
-            required: true
-          }
-        }
+export default {
+  props: {
+    post: {
+      type: Object,
+      required: true
     }
+  }
+};
 </script>
 
 <style scoped lang="scss">
-  .post-item {
-    padding: 1em;
-    border-radius: 5px;
-    border: 1px solid #eee;
-    margin-bottom: 1.3em;
+.post-item {
+  padding: 1em;
+  border-radius: 5px;
+  border: 1px solid #eee;
+  margin-bottom: 1.3em;
 
-    img {
-      max-width: 100%;
-    }
-
-    .title {
-      font-size: 24px;
-      margin-bottom: 20px;
-    }
+  img {
+    max-width: 100%;
   }
+
+  .title {
+    font-size: 24px;
+    margin-bottom: 20px;
+  }
+}
 </style>
--- pages/posts/_slug.vue
+++ pages/posts/_slug.vue
@@ -1,8 +1,27 @@
 <template>
-
+  <div>
+    <div v-if="loadingCurrent">loading...</div>
+    <div v-else class="post-item">
+      <img :src="post.fields.image.fields.file.url" />
+      <nuxt-link :to="'posts/' + post.fields.slug">
+        <h2 class="title">{{ post.fields.title }}</h2>
+      </nuxt-link>
+      <p v-html="post.fields.description"></p>
+    </div>
+  </div>
 </template>
 <script>
-  export default {
+import { mapGetters } from "vuex";
 
+export default {
+  asyncData({ store, route }) {
+    return store.dispatch({ type: "posts/loadPost", slug: route.params.slug });
+  },
+  computed: {
+    ...mapGetters({
+      post: "posts/currentPost",
+      loadingCurrent: "posts/loadingCurrentPost"
+    })
   }
+};
 </script>
