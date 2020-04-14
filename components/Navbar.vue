9232b6119caf04ddf9d01de2de6ddf82ad630f45
Nuno <n.alexandre@xgeeks.io>
2020-04-14 10:22:25 +0100

    add navbar
    Added a navbar with latest link, that will link to a page with the latests posts

---
+++ components/Navbar.vue
@@ -0,0 +1,31 @@
+<template>
+  <nav class="navbar is-transparent">
+    <div class="navbar-brand">
+      <nuxt-link class="navbar-item" to="/">
+        Nuxtful
+      </nuxt-link>
+      <div
+        class="navbar-burger burger"
+        data-target="navbarExampleTransparentExample"
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+
+    <div id="navbarExampleTransparentExample" class="navbar-end">
+      <div class="navbar-start">
+        <nuxt-link class="navbar-item" to="/posts">
+          Latest
+        </nuxt-link>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
