51c0fd7b24132f6155700b950fa4221c5df58491
Nuno <n.alexandre@xgeeks.io>
2020-04-15 13:57:16 +0100

    Latest posts page
    Added a latests page with posts displayed horizontally

---
+++ components/HorizontalPost.vue
@@ -0,0 +1,42 @@
+<template>
+  <section class="section">
+    <div class="card is-horizontal">
+      <div class="columns">
+        <div class="column is-4 card-image">
+          <figure class="image is-4by3">
+            <img :src="post.fields.image.fields.file.url" :alt="post.fields.image.fields.title" />
+          </figure>
+        </div>
+        <div class="column is-8">
+          <div class="card-content">
+            <nuxt-link :to="'/posts/' + post.fields.slug">
+              <h2 class="title is-4">{{ post.fields.title }}</h2>
+            </nuxt-link>
+            <div class="content">
+              <p v-html="post.fields.description"></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    post: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.card.is-horizontal {
+  .card-image {
+    padding: 0px;
+  }
+}
+</style>
---
+++ pages/posts/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div class="hero is-info ">
+      <div class="hero-body">
+        <h2 class="title">Latest Posts</h2>
+      </div>
+    </div>
+    <div class="container">
+      <div class="section">
+          <div
+            v-for="(post, index) in posts"
+            :key="'post-' + index"
+          >
+            <horizontal-post :post="post"></horizontal-post>
+          </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// import { getPosts } from "../content/posts";
+import { mapGetters } from 'vuex';
+import HorizontalPost from '../../components/HorizontalPost.vue';
+
+export default {
+  components: {
+    HorizontalPost
+  },
+  asyncData({ store }) {
+    return store.dispatch('posts/loadPosts');
+  },
+  computed: {
+    ...mapGetters({
+      posts: 'posts/posts'
+    })
+  }
+};
+</script>
+
+<style socped lang="scss"></style>
